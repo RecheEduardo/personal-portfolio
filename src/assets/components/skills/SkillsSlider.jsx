f5bd1c67570e749f4c46e1132722a6b24322568e
RecheEduardo <Eduardorechemartins2006@gmail.com>
2025-02-16 22:22:17 -0300

    feature: customização e interatividade do slider de conhecimentos concluído

--- src/assets/components/skills/SkillsSlider.jsx
+++ src/assets/components/skills/SkillsSlider.jsx
@@ -18,37 +18,79 @@ import { GrMysql } from "react-icons/gr";
 
 const SkillsSlider = () => {
   return (
-    <>
+    <div className="sliders">
       <div className="slider-left" 
             style={{"--quantity": 7, 
-                    "--width": '100px',
-                    "--height": '9rem' }
+                    "--width": '15rem',
+                    "--height": '15rem' }
        }>
         <div className="list">
-            <div className="item" style={{"--position": 1}}><FaHtml5 /></div>
-            <div className="item" style={{"--position": 2}}><FaJsSquare /></div>
-            <div className="item" style={{"--position": 3}}><FaPhp /></div>
-            <div className="item" style={{"--position": 4}}><FaGitAlt /></div>
-            <div className="item" style={{"--position": 5}}><FaAngular /></div>
-            <div className="item" style={{"--position": 6}}><FaBootstrap /></div>
-            <div className="item" style={{"--position": 7}}><FaReact /></div>
+            <div className="item" style={{"--position": 1}}>
+                <FaHtml5 />
+                <span>HTML</span>
+            </div>
+            <div className="item" style={{"--position": 2}}>
+                <FaJsSquare />
+                <span>JavaScript</span>
+            </div>
+            <div className="item" style={{"--position": 3}}>
+                <FaPhp />
+                <span>PHP</span>
+            </div>
+            <div className="item" style={{"--position": 4}}>
+                <FaGitAlt />
+                <span>Git</span>
+            </div>
+            <div className="item" style={{"--position": 5}}>
+                <FaAngular />
+                <span>Angular</span>
+            </div>
+            <div className="item" style={{"--position": 6}}>
+                <FaBootstrap />
+                <span>Bootstrap</span>
+            </div>
+            <div className="item" style={{"--position": 7}}>
+                <FaReact />
+                <span>React.Js</span>
+            </div>
         </div>
       </div>
       <div className="slider-right" style={{"--quantity": 7, 
-                    "--width": '100px',
-                    "--height": '9rem' }
+                    "--width": '15rem',
+                    "--height": '15rem'}
        }>
         <div className="list">
-            <div className="item" style={{"--position": 1}}><FaNodeJs /></div>
-            <div className="item" style={{"--position": 2}}><GrMysql /></div>
-            <div className="item" style={{"--position": 3}}><SiRubyonrails /></div>
-            <div className="item" style={{"--position": 4}}><FaSass /></div>
-            <div className="item" style={{"--position": 5}}><BiLogoPostgresql /></div>
-            <div className="item" style={{"--position": 6}}><BiLogoTypescript /></div>
-            <div className="item" style={{"--position": 7}}><SiSqlite /></div>
+            <div className="item" style={{"--position": 1}}>
+                <FaNodeJs />
+                <span>Node.Js</span>
+            </div>
+            <div className="item" style={{"--position": 2}}>
+                <GrMysql />
+                <span>MySQL</span>
+            </div>
+            <div className="item" style={{"--position": 3}}>
+                <SiRubyonrails />
+                <span>Ruby on Rails</span>
+            </div>
+            <div className="item" style={{"--position": 4}}>
+                <FaSass />
+                <span>SASS</span>
+            </div>
+            <div className="item" style={{"--position": 5}}>
+                <BiLogoPostgresql />
+                <span>PostgreSQL</span>
+            </div>
+            <div className="item" style={{"--position": 6}}>
+                <BiLogoTypescript />
+                <span>TypeScript</span>
+            </div>
+            <div className="item" style={{"--position": 7}}>
+                <SiSqlite />
+                <span>SQLite3</span>
+            </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
