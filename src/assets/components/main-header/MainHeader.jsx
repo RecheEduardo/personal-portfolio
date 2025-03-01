f10ffac8c58e6dd7c0a1afafb4947be24794da47
RecheEduardo <Eduardorechemartins2006@gmail.com>
2025-03-01 01:12:16 -0300

    implementando e testando mais componentes com o framer motion

--- src/assets/components/main-header/MainHeader.jsx
+++ src/assets/components/main-header/MainHeader.jsx
@@ -8,7 +8,13 @@ import { IoMdMail } from "react-icons/io";
 const MainHeader = () => {
   return (
     <section className='main-header' id='main-header'>
-      <div className="greetings-msg">
+      <motion.div className="greetings-msg"
+      initial={{ opacity: 0,  y: 150 }}
+      animate={{ opacity: 1,  y: 0}}
+      transition={{
+        opacity: { duration: 0.5 },
+        y: { type: "spring", stiffness: 150, damping: 13}
+      }}>
         <div className="greetings-title">
           <h1>Olá! Sou <span>Eduardo</span>, seja</h1>
           <h1><span>bem vindo</span> ao meu <span className="outline">mundo</span>...</h1>
@@ -21,14 +27,14 @@ const MainHeader = () => {
             <a href="https://wa.me/5511994088541?text=Ol%C3%A1%2C%20Eduardo!" className='media-btn' target='_blank'><FaWhatsapp/></a>
             <a href="mailto:eduardorechemartins2006@gmail.com" className='media-btn' target='_blank'><IoMdMail/></a>
         </div>
-      </div>
+      </motion.div>
       <motion.img src={Icon1} className="greetings-img"
-      variants={{
-        hidden: { opacity: 0, y: 150 },
-        show: { opacity: 1, y: 0, transition: { duration: 0.5 }},
-      }}
-      initial="hidden"
-      animate="show"/>
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        opacity: { duration: 0.5 },
+        scale: { type: "spring", stiffness: 50, damping: 10}
+      }}/>
     </section>
   )
 }
