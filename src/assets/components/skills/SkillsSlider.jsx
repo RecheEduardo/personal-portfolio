4ec857ed1f495c98116cc012a56d75e42cfdc3bf
RecheEduardo <Eduardorechemartins2006@gmail.com>
2025-03-01 17:26:01 -0300

    fix: otimizando a visualização do slider em dispositivos mobile

--- src/assets/components/skills/SkillsSlider.jsx
+++ src/assets/components/skills/SkillsSlider.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./SkillsSlider.css";
 /* Importando Logos para o Slider */
-import { FaHtml5,FaJsSquare,FaPhp,FaGitAlt,
-  FaAngular,FaBootstrap,FaNodeJs,FaSass,FaReact} from "react-icons/fa";
+import { FaHtml5, FaJsSquare, FaPhp, FaGitAlt,
+  FaAngular, FaBootstrap, FaNodeJs, FaSass, FaReact } from "react-icons/fa";
 import { BiLogoPostgresql, BiLogoTypescript } from "react-icons/bi";
 import { SiRubyonrails, SiSqlite } from "react-icons/si";
 import { GrMysql } from "react-icons/gr";
@@ -12,86 +12,78 @@ import { motion } from "framer-motion";
 
 const SkillsSlider = () => {
 
-    const contentVariants = {
-        hidden: { opacity: 0, scale: 0.5 },
-        show: { 
-            opacity: 1, 
-            scale: 1, 
-            transition: { 
-            opacity: { duration: 0.5, ease: "easeInOut" },
-            scale: { type: "spring", stiffness: 150, damping: 15 }
-            }
-        }
-    };
+  const contentVariants = {
+    hidden: { opacity: 0, scale: 0.5 },
+    show: { 
+      opacity: 1, 
+      scale: 1, 
+      transition: { 
+        opacity: { duration: 0.5, ease: "easeInOut" },
+        scale: { type: "spring", stiffness: 150, damping: 15 }
+      }
+    }
+  };
 
-    const topSlider = [
-        {icon: <FaHtml5 />, name: 'HTML', position: 1},
-        {icon: <FaJsSquare />, name: 'JavaScript', position: 2},
-        {icon: <FaPhp />, name: 'PHP', position: 3},
-        {icon: <FaGitAlt />, name: 'Git', position: 4},
-        {icon: <FaAngular />, name: 'Angular', position: 5},
-        {icon: <FaBootstrap />, name: 'Bootstrap', position: 6},
-        {icon: <FaReact />, name: 'React.Js', position: 7},
-    ]
+  const topSlider = [
+    { icon: <FaHtml5 />, name: 'HTML', position: 1 },
+    { icon: <FaJsSquare />, name: 'JavaScript', position: 2 },
+    { icon: <FaPhp />, name: 'PHP', position: 3 },
+    { icon: <FaGitAlt />, name: 'Git', position: 4 },
+    { icon: <FaAngular />, name: 'Angular', position: 5 },
+    { icon: <FaBootstrap />, name: 'Bootstrap', position: 6 },
+    { icon: <FaReact />, name: 'React.Js', position: 7 },
+  ];
 
-    const bottomSlider = [
-        {icon: <FaNodeJs />, name: 'Node.Js', position: 1},
-        {icon: <GrMysql />, name: 'MySQL', position: 2},
-        {icon: <SiRubyonrails />, name: 'Ruby on Rails', position: 3},
-        {icon: <FaSass />, name: 'SASS', position: 4},
-        {icon: <BiLogoPostgresql />, name: 'PostgreSQL', position: 5},
-        {icon: <BiLogoTypescript />, name: 'TypeScript', position: 6},
-        {icon: <SiSqlite />, name: 'SQLite3', position: 7},
-    ]
+  const bottomSlider = [
+    { icon: <FaNodeJs />, name: 'Node.Js', position: 1 },
+    { icon: <GrMysql />, name: 'MySQL', position: 2 },
+    { icon: <SiRubyonrails />, name: 'Ruby on Rails', position: 3 },
+    { icon: <FaSass />, name: 'SASS', position: 4 },
+    { icon: <BiLogoPostgresql />, name: 'PostgreSQL', position: 5 },
+    { icon: <BiLogoTypescript />, name: 'TypeScript', position: 6 },
+    { icon: <SiSqlite />, name: 'SQLite3', position: 7 },
+  ];
 
   return (
     <div className="sliders">
-        <motion.div className="slider-left" 
-            style={{"--quantity": 7, 
-                    "--width": '15rem',
-                    "--height": '15rem' }}
-            variants={contentVariants}
-            initial="hidden"
-            whileInView="show"
-            viewport={{
-                once: true,
-                margin: "-30% 0px -30% 0px"
-            }}
-        >
-            <div className="list">
-                {topSlider.map((badge, index) => {
-                        return(<SkillBadge key={index} 
-                            skillIcon={badge.icon} 
-                            skillName={badge.name} 
-                            position={badge.position}
-                        />)
-                    })
-                }
-            </div>
-        </motion.div>
+      <motion.div className="slider-left"
+        variants={contentVariants}
+        initial="hidden"
+        whileInView="show"
+        viewport={{
+          once: true,
+          margin: "-30% 0px -30% 0px"
+        }}
+      >
+        <div className="list">
+          {topSlider.map((badge, index) => (
+            <SkillBadge key={index} 
+              skillIcon={badge.icon} 
+              skillName={badge.name} 
+              position={badge.position}
+            />
+          ))}
+        </div>
+      </motion.div>
 
-        <motion.div className="slider-right" 
-            style={{"--quantity": 7, 
-                    "--width": '15rem',
-                    "--height": '15rem' }}
-            variants={contentVariants}
-            initial="hidden"
-            whileInView="show"
-            viewport={{
-                once: true,
-                margin: "-20% 0px -20% 0px"
-            }}
-        >
-            <div className="list">
-                {bottomSlider.map((badge, index) => {
-                        return(<SkillBadge key={index} 
-                            skillIcon={badge.icon} 
-                            skillName={badge.name} 
-                            position={badge.position}
-                        />)
-                    })
-                }
-            </div>
+      <motion.div className="slider-right"
+        variants={contentVariants}
+        initial="hidden"
+        whileInView="show"
+        viewport={{
+          once: true,
+          margin: "-20% 0px -20% 0px"
+        }}
+      >
+        <div className="list">
+          {bottomSlider.map((badge, index) => (
+            <SkillBadge key={index} 
+              skillIcon={badge.icon} 
+              skillName={badge.name} 
+              position={badge.position}
+            />
+          ))}
+        </div>
       </motion.div>
     </div>
   );
